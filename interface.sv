4cf299e33c7a25a4ce5c80af356669019f90d3dd
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-06-29 19:47:03 +0530

    added the interface.sv
    communicator between the test bench and DUT

---
+++ interface.sv
@@ -0,0 +1,13 @@
+interface intf;
+  logic a;
+  logic b;
+  logic c;
+  logic sum;
+  logic carry;
+  
+  // Clocking block
+  // modport
+  
+  // Whatever signal are signal are going inside the DUT we have to mentioned all inside interface
+  // as it is an combinational circuit we don't need these.
+endinterface
