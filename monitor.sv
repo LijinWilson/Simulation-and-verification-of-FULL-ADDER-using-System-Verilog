90720c0164aba9bffcd55f6df4545a15e1cb9595
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-06-30 00:29:25 +0530

    Add monitor class for DUT interface sampling
    Implemented a monitor class to sample values from the DUT interface and send transactions to the scoreboard via a mailbox.

---
+++ monitor.sv
@@ -0,0 +1,38 @@
+// Monitor Class: It will take the value from the interface coming from DUT and sending to scoreboard
+
+class monitor;
+  virtual intf vif;
+  
+//   MAILBOX for communication between the monitor and scoreboard
+  mailbox mon2scb;
+  
+//   Constructor
+  function new(virtual intf vif, mailbox mon2scb);
+    this.vif = vif;
+    this.mon2scb = mon2scb;
+  endfunction
+  
+//   MONITOR LOGIC
+  task main();
+    repeat(2)
+      #1; // give 1 time delay to sample the data
+    begin
+      transaction trans;
+      trans.new();	// creating the object of the transaction class
+      
+      // Sampling
+      trans.a = vif.a;
+      trans.b = vif.b;
+      trans.c = vif.c;
+      trans.sum = vif.sum;
+      trans.carry = vif.carry;
+      
+//       Putting all the value inside the mail box
+      mon2scb.put(trans);
+      
+//       Displaying all the value inserted
+      trans.display("Monitor Class Signals");
+    end
+  endtask
+  
+endclass
