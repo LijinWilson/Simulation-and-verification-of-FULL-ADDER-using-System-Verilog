f2940c5891977928b58043d67a7f745a6617f4c0
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-06-30 18:44:17 +0530

    Add scoreboard class for DUT output validation
    Implement scoreboard class to validate DUT outputs using mailbox.

---
+++ scoreboard.sv
@@ -0,0 +1,40 @@
+/*
+  -  It will collect the data/packet using the MAIL BOX from MONITOR.
+  -  It will check whatever output sending from the DUT through monitor are correct or not.
+  
+*/
+
+class scoreboard;
+
+//   MAIL BOX to collect the data from the DUT to monitor
+  mailbox mon2scb;
+  
+//   Constructor
+  function new(mailbox mon2scb);
+    this.mon2scb = mon2scb;
+  endfunction
+  
+//   scoreboard logic
+  task main();
+    transaction trans;
+    
+    repeat(2)
+      begin
+//         Getting the value stores inside the mail box
+        mon2scb.get(trans);
+        trans.display("Score Board Signals");
+        
+//         Validation cases/logic
+        if(((trans.a ^ trans.b ^ trans.c) == trans.sum) && ((trans.a & trans.b)|(trans.b & trans.c) | (trans.a & trans.c) == trans.carry))
+          begin
+            $display("**************** PASS ****************")
+          end
+        else
+          begin
+            $display("**************** FAIL ****************");
+          end
+        $display("///////////////////// TRANSACTION DONE /////////////////////");.
+      end
+  endtask
+  
+endclass
