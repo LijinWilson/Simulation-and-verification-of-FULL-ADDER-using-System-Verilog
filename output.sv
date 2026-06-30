df0823bfd3c8d3e7a0118468c926e86533d502cf
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-06-30 23:37:38 +0530

    Add simulation output to output.sv
    Added simulation output and status messages.

---
+++ output.sv
@@ -0,0 +1,16 @@
+# KERNEL: Generator class variables
+# KERNEL: a = 0 | b = 1 | c = 0 | sum = 0 | carry = 0
+# KERNEL: Generator class variables
+# KERNEL: a = 1 | b = 0 | c = 1 | sum = 0 | carry = 0
+# KERNEL: Driver Class Signals
+# KERNEL: a = 0 | b = 1 | c = 0 | sum = 0 | carry = 0
+# KERNEL: Driver Class Signals
+# KERNEL: a = 1 | b = 0 | c = 1 | sum = 0 | carry = 0
+# KERNEL: Monitor Class Signals
+# KERNEL: a = 1 | b = 0 | c = 1 | sum = 0 | carry = 1
+# KERNEL: Score Board Signals
+# KERNEL: a = 1 | b = 0 | c = 1 | sum = 0 | carry = 1
+# KERNEL: **************** PASS ****************
+# KERNEL: ///////////////////// TRANSACTION DONE /////////////////////
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
