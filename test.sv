dc4e72d68c977a4ed135ddfcc34ab4258956ba77
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-06-30 19:16:42 +0530

    Add test program to execute environment block
    this is the main block in which the DUT and interface are connected

---
+++ test.sv
@@ -0,0 +1,15 @@
+// Inside this the environment block is going to be executed
+
+// Including the environment component
+`include environment.sv;
+
+program test(intf intff);
+  environment env;
+  
+  initial
+    begin
+      env = new(intff);
+      env.test_run();
+    end
+  
+endprogram
